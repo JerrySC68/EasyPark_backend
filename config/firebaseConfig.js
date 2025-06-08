a10ad62027ce74ac2f3732938a2e5602637f5e24
Josue Vindas <josue.vindas.perez@est.una.ac.cr>
2025-06-08 15:46:38 -0600

    `Update firebaseConfig.js to use process.env.FIREBASE_SERVICE_ACCOUNT`

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,8 +1,11 @@
 // config/firebaseConfig.js
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
+//const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
+// 🔥 Esta línea es clave para que funcione bien la private_key
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
-serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
