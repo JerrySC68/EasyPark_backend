e0ed0e60f52b1afdf126993428e8612518e28f84
Josue Vindas <josue.vindas.perez@est.una.ac.cr>
2025-06-08 15:38:32 -0600

    `Update firebaseConfig.js to use environment variable for service account key`

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,6 +1,8 @@
 // config/firebaseConfig.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const admin = require("firebase-admin");
+
+
+serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
